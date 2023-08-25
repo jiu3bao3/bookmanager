4fd5d0a2123169264ac788aa34d697e0aa936c65
KUBO, Yoshihito <kubo@jy-info-sys.com>
2023-08-25 21:32:06 +0900

    一応APIから書誌情報を取得できる

--- src/main/java/com/jyis/bookmanager/ndl/NdlBookItemProcessor.java
+++ src/main/java/com/jyis/bookmanager/ndl/NdlBookItemProcessor.java
@@ -1,10 +1,21 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 package com.jyis.bookmanager.ndl;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.jyis.bookmanager.books.Book;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,11 +27,52 @@ public class NdlBookItemProcessor implements ItemProcessor<Book, NdlInfo>
 {
     /** ロガー */
     private static final Logger logger = LoggerFactory.getLogger(NdlBookItemProcessor.class);
+
+    private static final String NDL_API_ENDPOINT = "https://api.openbd.jp/v1/get?isbn=%s";
     //---------------------------------------------------------------------------------------------
     @Override
     public NdlInfo process(final Book book) throws Exception
     {
-        logger.info(book.toString());
+        getNdlInfo(book.getIsbn());
         return new NdlInfo();
+    }
+    private void getNdlInfo(final String isbn)
+    {
+        if(isbn == null) return;
+        try 
+        {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(String.format(NDL_API_ENDPOINT, isbn)))
+                            .build();
+            HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+            HttpResponse<String> response = client.send(request, handler);
+            logger.info("STATUS=" + response.statusCode());
+            if(response.statusCode() == 200)
+            {
+                HttpHeaders headers = response.headers();
+                Optional<String> contentType = headers.firstValue("Content-Type");
+                contentType.ifPresent(c -> logger.info(c));
+                String body = response.body();
+                logger.info(body);
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonRoot = mapper.readTree(body);
+                JsonNode firstNode = jsonRoot.get(0);
+                if(firstNode != null)
+                {
+                    JsonNode onixNode = firstNode.get("onix");
+                    if(onixNode != null)
+                    {
+                        logger.info(onixNode.get("CollateralDetail").toString());
+                    }
+                }
+                
+            }
+            Thread.sleep(100);
+        }
+        catch(IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
